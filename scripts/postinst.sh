11e843b47fd0f1711f8d86d43f82afc273984af7
tang <tanguy.bonneau@gmail.com>
2024-10-15 19:47:24 +0200

    Update install script

--- scripts/postinst.sh
+++ scripts/postinst.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
-# install smbus lib
-apt update -qq && apt install -q --yes python3-smbus
-if [ $? -ne 0 ]; then
-    echo "Error installing python3-smbus package"
-    exit 1
-fi
-
 # install Four Letter pHAT driver from Pimoroni
-python3 -m pip install --trusted-host pypi.org "fourletterphat>=0.1.0"
+python3 -m pip install --trusted-host pypi.org "fourletterphat>=0.1.0" "smbus"
 if [ $? -ne 0 ]; then
     echo "Error installing Four Letter pHAT driver"
     exit 1
